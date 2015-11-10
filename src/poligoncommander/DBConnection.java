598b7e495b59abb07a8bc407121f420ee2481fe5
Igor Klekotnev <klekotnev@gmail.com>
2015-11-10 16:46:41 +0200

    Initial commit

---
+++ src/poligoncommander/DBConnection.java
@@ -0,0 +1,17 @@
+/*
+ * 
+ * 
+ */
+package poligoncommander;
+
+/**
+ *
+ * @author kataev
+ */
+public class DBConnection {
+    // JDBC URL, username and password of MySQL server
+    private static final String dbUrl = "jdbc:mysql://localhost:3306/poligon";
+    private static final String user = "root";
+    private static final String password = "poligon";
+  
+}
